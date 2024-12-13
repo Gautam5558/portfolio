86071ed9eee9cd9f5846205e80e85d236ac9aad9
Gautam Jain <gautamjain5558@gmail.com>
2024-12-13 11:59:10 +0530

    Completed Experience section

---
+++ src/components/Experience.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import Image from "next/image";
+import Heading from "./reusable/Heading";
+import { arrowLeftIcon, experienceData } from "@/assets";
+
+const Experience = () => {
+  const date = new Date().getFullYear();
+  return (
+    <div className="relative py-20 px-96">
+      <Heading text="Experience & Education" />
+      <Image
+        src={"/education.png"}
+        alt="Experience Image"
+        width={400}
+        height={400}
+        className="absolute -top-4 right-96 opacity-70 lg:hidden"
+      />
+
+      <div className="w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10">
+        {experienceData.map((item, i) => {
+          return (
+            <div
+              key={i}
+              className={`w-[600px] xl:w-[480px] sm:w-full px-12 sm:px-0 relative ${
+                i % 2 === 0
+                  ? "-left-[300px] xl:-left-[240px] lg:-left-0"
+                  : "left-[300px] xl:left-[240px] lg:left-0"
+              }`}
+            >
+              <div className=" relative flex flex-col gap-y-3 rounded-md border border-red-300 bg-white p-4 tracking-wide sm:text-sm">
+                <h1 className="text-xl sm:text-lg font-light text-gray-700">
+                  {item.title}
+                </h1>
+                <p className="text-gray-800">
+                  <span className="font-light block">Education:</span>
+                  <span className="block font-extralight pl-2">
+                    {item.education}
+                  </span>
+                </p>
+                <div className="text-gray-800">
+                  <span className="font-light">Experience:</span>
+                  <ul className="pl-2">
+                    {item.experience.map((exp, j) => {
+                      return (
+                        <li key={j} className="my-1 font-extralight">
+                          {exp}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+                <span
+                  className={`absolute top-20 text-red-300 -translate-y-1/2 ${
+                    i % 2 === 0 ? "left-full rotate-180" : "right-full"
+                  }`}
+                >
+                  {arrowLeftIcon}
+                </span>
+              </div>
+              <div
+                className={`absolute top-20 w-14 z-10 bg-white border border-gray-300 rounded-full aspect-square grid place-items-center text-red-400 font-light -translate-y-1/2 ${
+                  i % 2 === 0
+                    ? "left-full -translate-x-1/2 lg:left-1/2"
+                    : "right-full translate-x-1/2 lg:right-1/2"
+                }`}
+              >
+                {date - experienceData.length + i + 1}
+              </div>
+            </div>
+          );
+        })}
+        <div className="absolute w-1 h-full rounded-full bg-gray-300" />
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
