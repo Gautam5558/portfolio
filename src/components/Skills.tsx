7b06327885e27201c9be09b94d44b402461fad3d
Gautam Jain <gautamjain5558@gmail.com>
2024-12-15 15:43:35 +0530

    Added animation to skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,17 +4,39 @@ import React from "react";
 import Heading from "./reusable/Heading";
 import Image from "next/image";
 import { skillsData } from "@/assets";
+import { motion } from "framer-motion";
 
 const Skills = () => {
+  const variants = {
+    visible: (i: number) => {
+      return {
+        opacity: 1,
+        y: 0,
+        transition: {
+          delay: 0.3 + i * 0.07,
+        },
+      };
+    },
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+  };
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-y-20 px-96">
       <Heading text="Skills" />
       <div className="w-full flex justify-between flex-wrap gap-x-8 gap-y-10 lg:gap-y-6">
         {skillsData.map((item, index) => {
           return (
-            <div
+            <motion.div
+              custom={index}
+              variants={variants}
+              initial="hidden"
+              whileInView="visible"
+              whileHover={{ scale: 1.1 }}
+              viewport={{ margin: "50px", once: true }}
               key={index}
-              className="flex items-center justify-center gap-x-3 rounded-xl border border-yellow-800 bg-zinc-200 px-5 py-2 lg:px-2"
+              className="flex items-center justify-center gap-x-3 rounded-xl border border-yellow-500 bg-zinc-200 px-5 py-2 lg:px-2"
             >
               <Image
                 src={item.icon}
@@ -24,7 +46,7 @@ const Skills = () => {
                 className="h-auto w-[40px]"
               />
               <p className="text-sm text-gray-600">{item.name}</p>
-            </div>
+            </motion.div>
           );
         })}
       </div>
