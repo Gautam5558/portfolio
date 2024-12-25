07377cbf14e77ebdaa781c98a16c9fd35a4ca7f0
Gautam Jain <gautamjain5558@gmail.com>
2024-12-25 16:02:29 +0530

    Added animation to contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,13 +2,19 @@
 
 import Image from "next/image";
 import Heading from "./reusable/Heading";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="h-screen py-20 px-56">
       <Heading text="Get in touch" />
       <div className="w-full h-full my-auto flex lg:flex-col items-center justify-between lg:jusify-center gap-x-20 lg:gap-x-0 gap-y-20">
-        <div>
+        <motion.div
+          initial={{ y: 150, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+        >
           <Image
             src={"/contact.gif"}
             alt="contact-section-image"
@@ -16,8 +22,14 @@ const Contact = () => {
             height={400}
             className="w-[400px] rounded-md opacity-80"
           />
-        </div>
-        <form className="w-[600px] lg:w-[400px] sm:w-full flex flex-col gap-3">
+        </motion.div>
+        <motion.form
+          initial={{ x: 150, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+          className="w-[600px] lg:w-[400px] sm:w-full flex flex-col gap-3"
+        >
           <div className="w-full flex lg:flex-col gap-x-3 lg:gap-y-3">
             <input
               type="text"
@@ -42,7 +54,7 @@ const Contact = () => {
           <button className="w-full border border-yellow-500 rounded-md bg-yellow-600 px-4 py-2 text-sm font-light tracking-wider text-white outine-none hover:bg-yellow-500 transition-colors cursor-pointer">
             Send Message
           </button>
-        </form>
+        </motion.form>
       </div>
     </div>
   );
